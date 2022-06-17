b27fcc2d0a817eba53a1fce88e2b56abf4610548
masashi-toda <toda414@gmail.com>
2022-06-18 06:18:13 +0900

    add accesslog middleware

---
+++ middleware/accesslog/accesslog.go
@@ -0,0 +1,95 @@
+package accesslog
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gotech-labs/api"
+	"github.com/gotech-labs/core/log"
+	"github.com/gotech-labs/core/system"
+	"github.com/rs/zerolog"
+)
+
+func New(writer io.Writer) *accessLog {
+	return &accessLog{
+		loggingFilter:        func(_ string) bool { return false },
+		loggingReqBodyFilter: func(api.Request) bool { return false },
+		logger:               log.New(writer),
+	}
+}
+
+type accessLog struct {
+	loggingFilter        func(string) bool
+	loggingReqBodyFilter func(api.Request) bool
+	logger               *log.Logger
+}
+
+func (mw *accessLog) Middleware(next api.HandlerFunc) api.HandlerFunc {
+	return func(ctx context.Context, req api.Request) (resp api.Response) {
+		var (
+			body []byte
+		)
+		if mw.loggingFilter(req.Path()) {
+			if mw.loggingReqBodyFilter(req) && req.ContentLength() > 0 {
+				body = req.Body()
+			}
+			defer func(begin time.Time) {
+				evt := mw.logEvent(resp.Status()).
+					Int("status", resp.Status()).
+					Str("method", req.Method()).
+					Str("path", req.Path()).
+					Interface("query", req.QueryParameters()).
+					Interface("header", req.Headers()).
+					Str("protocol", req.Protocol()).
+					Str("client_ip", req.ClientIP()).
+					Str("useragent", req.UserAgent()).
+					Str("referer", req.Referer()).
+					Dur("latency", system.CurrentTime().Sub(begin)).
+					Str("target", req.Host()).
+					Str("server", host)
+				if len(body) > 0 {
+					evt = evt.RawJSON("body", body)
+				}
+				// write access log
+				evt.Send()
+			}(system.CurrentTime())
+		}
+		// call next handler function
+		return next(ctx, req)
+	}
+}
+
+func (mw *accessLog) WithSkipPath(paths ...string) *accessLog {
+	mw.loggingFilter = func(target string) bool {
+		for _, path := range paths {
+			if path == target {
+				return false
+			}
+		}
+		return true
+	}
+	return mw
+}
+
+func (mw *accessLog) WithLoggingReqBodyFilter(filter func(api.Request) bool) *accessLog {
+	mw.loggingReqBodyFilter = filter
+	return mw
+}
+
+func (mw *accessLog) logEvent(status int) *zerolog.Event {
+	switch {
+	case status >= http.StatusBadRequest && status < http.StatusInternalServerError:
+		return mw.logger.Warn()
+	case status >= http.StatusInternalServerError:
+		return mw.logger.Error()
+	default:
+		return mw.logger.Info()
+	}
+}
+
+var (
+	host, _ = os.Hostname()
+)
---
+++ middleware/accesslog/accesslog_test.go
@@ -0,0 +1,287 @@
+package accesslog_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	apitest "github.com/gotech-labs/api/testing"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gotech-labs/api"
+	. "github.com/gotech-labs/api/middleware/accesslog"
+	"github.com/gotech-labs/core/system"
+)
+
+func TestAccessLog(t *testing.T) {
+	var (
+		host, _ = os.Hostname()
+	)
+	system.RunTest(t, "skip logging", func(t *testing.T) {
+		var (
+			rb = apitest.RequestBuilder{
+				Method: http.MethodGet,
+				Path:   "/search",
+				Body: []byte(`{
+					"keyword": "hello"
+				}`),
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+					"X-Request-Id": "req-12345",
+					"User-Agent":   "TestUserAgent",
+				},
+				QueryParams: map[string]string{
+					"req_id": "req-XXXXXXXXXXXXXXXXX",
+					"pretty": "true",
+				},
+			}
+			req      = api.NewRequest(rb.Build())
+			buf      = bytes.NewBuffer(nil)
+			skipPath = []string{rb.Path} // skip path
+			filter   = func(req api.Request) bool {
+				return true
+			}
+			handler = func(ctx context.Context, req api.Request) api.Response {
+				return api.OK(map[string]string{"message": "ok"})
+			}
+		)
+		// call middleware function
+		resp := New(buf).
+			WithSkipPath(skipPath...).
+			WithLoggingReqBodyFilter(filter).
+			Middleware(handler)(context.Background(), req)
+		assert.Equal(t, http.StatusOK, resp.Status())
+
+		// assert log message
+		assert.Empty(t, buf.String())
+	})
+
+	system.RunTest(t, "ok response (without request body)", func(t *testing.T) {
+		var (
+			rb = apitest.RequestBuilder{
+				Method: http.MethodPost,
+				Path:   "/search",
+				Body: []byte(`{
+					"keyword": "hello"
+				}`),
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+					"X-Request-Id": "req-12345",
+					"User-Agent":   "TestUserAgent",
+					"Referer":      "TestReferer",
+				},
+				QueryParams: map[string]string{
+					"req_id": "req-XXXXXXXXXXXXXXXXX",
+					"pretty": "true",
+				},
+			}
+			req      = api.NewRequest(rb.Build())
+			buf      = bytes.NewBuffer(nil)
+			skipPath = []string{}
+			filter   = func(req api.Request) bool {
+				return false
+			}
+			handler = func(ctx context.Context, req api.Request) api.Response {
+				return api.OK(map[string]string{"message": "ok"})
+			}
+		)
+		// call middleware function
+		resp := New(buf).
+			WithSkipPath(skipPath...).
+			WithLoggingReqBodyFilter(filter).
+			Middleware(handler)(context.Background(), req)
+		assert.Equal(t, http.StatusOK, resp.Status())
+
+		expected := fmt.Sprintf(`{
+			"level": "info",
+			"time": "2022-12-24T00:00:00+09:00",
+			"server": "%s",
+			"status": 200,
+			"method": "POST",
+			"path": "/search",
+			"query": {
+				"pretty": "true",
+				"req_id": "req-XXXXXXXXXXXXXXXXX"
+			},
+			"protocol": "HTTP/1.1",
+			"client_ip": "127.0.0.1",
+			"header": {
+				"Content-Type": "application/json",
+				"X-Request-Id": "req-12345",
+				"User-Agent":   "TestUserAgent",
+				"Referer":      "TestReferer"
+			},
+			"useragent": "TestUserAgent",
+			"referer": "TestReferer",
+			"latency": 0,
+			"target": "localhost"
+		}`, host)
+		// assert log message
+		assert.JSONEq(t, expected, buf.String())
+	})
+
+	system.RunTest(t, "ok response (with body)", func(t *testing.T) {
+		var (
+			rb = apitest.RequestBuilder{
+				Method: http.MethodPost,
+				Path:   "/search",
+				Body:   []byte(`{"keyword": "hello"}`),
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+					"X-Request-Id": "req-12345",
+					"User-Agent":   "TestUserAgent",
+					"Referer":      "TestReferer",
+				},
+				QueryParams: map[string]string{
+					"req_id": "req-XXXXXXXXXXXXXXXXX",
+					"pretty": "true",
+				},
+			}
+			req      = api.NewRequest(rb.Build())
+			buf      = bytes.NewBuffer(nil)
+			skipPath = []string{}
+			filter   = func(req api.Request) bool {
+				return true
+			}
+			handler = func(ctx context.Context, req api.Request) api.Response {
+				return api.OK(map[string]string{"message": "ok"})
+			}
+		)
+		// call middleware function
+		resp := New(buf).
+			WithSkipPath(skipPath...).
+			WithLoggingReqBodyFilter(filter).
+			Middleware(handler)(context.Background(), req)
+		assert.Equal(t, http.StatusOK, resp.Status())
+
+		expected := fmt.Sprintf(`{
+			"level": "info",
+			"time": "2022-12-24T00:00:00+09:00",
+			"server": "%s",
+			"status": 200,
+			"method": "POST",
+			"path": "/search",
+			"query": {
+				"pretty": "true",
+				"req_id": "req-XXXXXXXXXXXXXXXXX"
+			},
+			"body": {
+				"keyword": "hello"
+			},
+			"protocol": "HTTP/1.1",
+			"client_ip": "127.0.0.1",
+			"header": {
+				"Content-Type": "application/json",
+				"X-Request-Id": "req-12345",
+				"User-Agent":   "TestUserAgent",
+				"Referer":      "TestReferer"
+			},
+			"useragent": "TestUserAgent",
+			"referer": "TestReferer",
+			"latency": 0,
+			"target": "localhost"
+		}`, host)
+		// assert log message
+		assert.JSONEq(t, expected, buf.String())
+	})
+
+	system.RunTest(t, "bad request response", func(t *testing.T) {
+		var (
+			rb = apitest.RequestBuilder{
+				Method: http.MethodGet,
+				Path:   "/search",
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+			}
+			req      = api.NewRequest(rb.Build())
+			buf      = bytes.NewBuffer(nil)
+			skipPath = []string{}
+			filter   = func(req api.Request) bool {
+				return true
+			}
+			handler = func(ctx context.Context, req api.Request) api.Response {
+				return api.BadRequest(errors.New("bad request"))
+			}
+		)
+		// call middleware function
+		resp := New(buf).
+			WithSkipPath(skipPath...).
+			WithLoggingReqBodyFilter(filter).
+			Middleware(handler)(context.Background(), req)
+		assert.Equal(t, http.StatusBadRequest, resp.Status())
+
+		expected := fmt.Sprintf(`{
+			"level": "warn",
+			"time": "2022-12-24T00:00:00+09:00",
+			"server": "%s",
+			"status": 400,
+			"method": "GET",
+			"path": "/search",
+			"query": {},
+			"protocol": "HTTP/1.1",
+			"client_ip": "127.0.0.1",
+			"header": {
+				"Content-Type": "application/json"
+			},
+			"useragent": "",
+			"referer": "",
+			"latency": 0,
+			"target": "localhost"
+		}`, host)
+		// assert log message
+		assert.JSONEq(t, expected, buf.String())
+	})
+
+	system.RunTest(t, "internal server error response", func(t *testing.T) {
+		var (
+			rb = apitest.RequestBuilder{
+				Method: http.MethodGet,
+				Path:   "/search",
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+			}
+			req      = api.NewRequest(rb.Build())
+			buf      = bytes.NewBuffer(nil)
+			skipPath = []string{}
+			filter   = func(req api.Request) bool {
+				return false
+			}
+			handler = func(ctx context.Context, req api.Request) api.Response {
+				return api.InternalServerError(errors.New("unexpected error"))
+			}
+		)
+		// call middleware function
+		resp := New(buf).
+			WithSkipPath(skipPath...).
+			WithLoggingReqBodyFilter(filter).
+			Middleware(handler)(context.Background(), req)
+		assert.Equal(t, http.StatusInternalServerError, resp.Status())
+
+		expected := fmt.Sprintf(`{
+			"level": "error",
+			"time": "2022-12-24T00:00:00+09:00",
+			"server": "%s",
+			"status": 500,
+			"method": "GET",
+			"path": "/search",
+			"query": {},
+			"protocol": "HTTP/1.1",
+			"client_ip": "127.0.0.1",
+			"header": {
+				"Content-Type": "application/json"
+			},
+			"useragent": "",
+			"referer": "",
+			"latency": 0,
+			"target": "localhost"
+		}`, host)
+		// assert log message
+		assert.JSONEq(t, expected, buf.String())
+	})
+}
